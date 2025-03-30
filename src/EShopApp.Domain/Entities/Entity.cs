a8a75af4c2367eac91205a9573845731f5df016f
masaftic <yousefalaa618y@gmail.com>
2025-03-30 03:37:31 +0200

    Fix nullable reference warning in Entity base class constructor

--- src/EShopApp.Domain/Entities/Entity.cs
+++ src/EShopApp.Domain/Entities/Entity.cs
@@ -4,7 +4,9 @@ public abstract class Entity<TId>
 {
     public TId Id { get; protected set; }
 
+#pragma warning disable CS8618
     protected Entity()
+#pragma warning restore CS8618
     {
     }
 }
