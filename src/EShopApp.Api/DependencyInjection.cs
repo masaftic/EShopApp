20bc6b0397862158a26cf9b2e17536aa57bab412
masaftic <yousefalaa618y@gmail.com>
2025-04-11 10:05:38 +0200

    Add JWT authentication support in Swagger configuration

--- src/EShopApp.Api/DependencyInjection.cs
+++ src/EShopApp.Api/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using ErrorOr;
+using Microsoft.OpenApi.Models;
 
 namespace EShopApp.Api;
 
@@ -8,7 +9,35 @@ public static class DependencyInjection
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(c =>
+        {
+            // Add JWT Authentication
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Enter 'Bearer' [space] and then your token"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
         services.AddControllers()
             .AddJsonOptions(opt => { opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
 
